b6d35545ad1b5ec9aced4e4a3ed75d2eb401f80b
Musa Yohanes <musayohanes00@gmail.com>
2023-04-14 18:23:03 +0700

    add url and access page

--- scraper.py
+++ scraper.py
@@ -1,2 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
+
+url = "https://www.rumah123.com/sewa/bandung/rumah/"
+page = requests.get(url)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+listings = soup.find_all("div", class_="card-featured__content-wrapper")
+
+# for section in listings:
+#     title = section.find("a", )
