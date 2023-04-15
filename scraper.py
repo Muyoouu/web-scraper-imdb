52e090f468bebad293ef0f5e74f5607f0169b62e
Musa Yohanes <musayohanes00@gmail.com>
2023-04-15 18:09:32 +0700

    add header to resolve get requests denied 403

--- scraper.py
+++ scraper.py
@@ -2,11 +2,9 @@ from bs4 import BeautifulSoup
 import requests
 
 url = "https://www.rumah123.com/sewa/bandung/rumah/"
-page = requests.get(url)
+page = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"})
 
 soup = BeautifulSoup(page.content, "html.parser")
 
 listings = soup.find_all("div", class_="card-featured__content-wrapper")
 
-# for section in listings:
-#     title = section.find("a", )
