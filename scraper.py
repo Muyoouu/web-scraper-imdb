8c8daf8e88ac083b6dd82eee40812ea9bb7cc8b3
Musa Yohanes <musayohanes00@gmail.com>
2023-04-15 18:13:50 +0700

    scrape selected data in listings

--- scraper.py
+++ scraper.py
@@ -8,3 +8,8 @@ soup = BeautifulSoup(page.content, "html.parser")
 
 listings = soup.find_all("div", class_="card-featured__content-wrapper")
 
+for section in listings:
+    title = section.find("a")["title"]
+    location = section.find("span").text
+    price = section.find("strong").text
+    description = section.find("p").text
